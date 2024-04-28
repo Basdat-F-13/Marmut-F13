e8b2e2f3ed52bf029bc8fd16bbca9d6c64e85250
ghania.larasati <ghania.larasati@ui.ac.id>
2024-04-28 22:33:20 +0700

    menyelesaikan front end dashboard

--- Marmut_F13/settings.py
+++ Marmut_F13/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Authentication',
-
-
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'dashboard/assets'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
