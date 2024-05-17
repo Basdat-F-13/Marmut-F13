cc96715749d60e3d5599af043979a8aa64338637
RafGhan <rgharditama@gmail.com>
2024-05-17 17:29:46 +0700

    resolve merge issue

--- Marmut_F13/urls.py
+++ Marmut_F13/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authentication/', include('Authentication.urls')),
+    path('album/', include('SongAlbum.urls')),
     path('manageplaylist/', include('manageplaylist.urls')),
 ]
