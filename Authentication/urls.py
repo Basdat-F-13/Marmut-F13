64470f27a1b2e1da79e0bcd017896636ffb894f5
kelvin-saputra <kelvin.saputra@ui.ac.id>
2024-05-17 20:05:03 +0700

    Authentication finish

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -4,5 +4,11 @@ from Authentication.views import *
 app_name = 'Authentication'
 
 urlpatterns = [
-    path('', showLoginPage, name='login'),
+    path('', showAuthNav, name='auth_nav'),
+    path('login', login, name='login'),
+    path('nav', showNav, name='nav'),
+    path('logout', logout, name='logout'),
+    path('register',showRegister, name='register'),
+    path('register-user',showUserRegPage,name='register-user'),
+    path('register-label',showLabelRegPage, name='register-label')
 ]
