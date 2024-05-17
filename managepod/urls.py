f1b02dfdf2d0bfb94635ade2661d9d0b24af4268
ghania.larasati <ghania.larasati@ui.ac.id>
2024-05-17 20:19:12 +0700

    fetching data for managepod

--- managepod/urls.py
+++ managepod/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from managepod.views import *
 
 app_name = 'managepod'
 
 urlpatterns = [
     path('managepod', showmanagepod, name='managepod'),
-    path('list', showlist, name='list'),
+    re_path(r'^list/(?P<podcast_id>[0-9a-f-]+)/$', showlist, name='list'),
     path('createpod', showcreatepod, name='createpod'),
-    path('addepisode', showaddepisode, name='addepisode'),
+    re_path(r'^addepisode/(?P<podcast_id>[0-9a-f-]+)/$', showaddepisode, name='addepisode'),
+    re_path(r'^deletepodcast/(?P<podcast_id>[0-9a-f-]+)/$', delete_podcast, name='delete_podcast'),
+    re_path(r'^deleteepisode/(?P<episode_id>[0-9a-f-]+)/(?P<podcast_id>[0-9a-f-]+)/$', delete_episode, name='delete_episode'),
 ]
